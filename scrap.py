26764bdac655040b53e496e21d3a7b18c5a7e4bb
frans ferdinand kbarek <franzferdinand.kbarek@gmail.com>
2019-05-18 19:26:16 +0700

    write mega menu links to txt file

--- scrap.py
+++ scrap.py
@@ -1,16 +1,23 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-page                          = get('http://www.nanokomputer.com/', {}, stream=True)
-parsed_page                   = BeautifulSoup(page.content, 'html.parser')
-all_link_with_link_lev_class  = parsed_page.select('a[class^=link_lev_]')
+import os
 
 
-def get_product_link(product_element_list):
+url = 'http://www.nanokomputer.com/'
 
-  link  = []
+
+def get_homepage(url):
+  homepage  = get(url, {}, stream=True)
+  parsed    = BeautifulSoup(homepage.content, 'html.parser')
+  return parsed
+
+
+def get_product_link(homepage):
+  all_link_with_link_lev_class  = homepage.select('a[class^=link_lev_]')
+  link                          = []
   
-  for i in product_element_list:
+  for i in all_link_with_link_lev_class:
   
     if 'daddy' in i['class']:
       continue
@@ -18,9 +25,33 @@ def get_product_link(product_element_list):
       link.append(i['href'])
   
   return link
+
+
+def write_megamenu_link_txt(list, filename):
+  # os.remove(filename)
+  f = open(filename, "a")
   
+  for i in list:
+    if list.index(i) < len(list)-1:
+      f.write(i+"\n")
+    else:
+      f.write(i)
 
-result = get_product_link(all_link_with_link_lev_class)
+  f.close()
 
-print(result)
-print(len(result))
+
+def get_product_list(txtfile):
+  if os.path.exists(txtfile):
+    print("EXISTS")
+  else:
+    result = get_product_link(get_homepage(url))
+    write_megamenu_link_txt(result, txtfile)
+    print("CREATED")
+
+
+
+get_product_list("megamenu-link.txt")
+
+
+# print(result)
+# print(len(result))
